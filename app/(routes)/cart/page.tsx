159426e3f78371dc72a394b869d009e3f1acf060
percy <p3r.valderrama@gmail.com>
2024-09-05 13:00:24 -0500

    feat-navbar-shoppiing

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -2,7 +2,7 @@
 
 const Page = () => {
   return <div>
-    dad
+    card
   </div>
 }
 
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,14 @@
 "use client"
 
-import { Heart, ShoppingCart, User } from "lucide-react"
+import { BaggageClaim, Heart, ShoppingCart, User } from "lucide-react"
 import { useRouter } from "next/navigation"
 import MenuList from "./menu-list"
 import ItemsMenuMobile from "./items-menu-mobile"
 import ToggleTheme from "./toggle-theme"
+import { useCart } from "@/hooks/use-cart"
 const Navbar = () => {
     const router = useRouter()
+    const cart = useCart()
     return(
         <div className="flex items-center justify-between p-4 mx-auto cursor-pointer sm:max">
             <h1 className="text-3xl "onClick={() => router.push("/")}>Caffe
@@ -20,7 +22,17 @@ const Navbar = () => {
                 <ItemsMenuMobile />
             </div>
             <div className="flex items-center justify-between gap-2 sm:gap-7">
-                <ShoppingCart  strokeWidth="1" className="cursor-pointer " onClick={() => router.push("/cart")}/>
+                {cart.items.length == 0 ? (
+                    <ShoppingCart  strokeWidth="1" className="cursor-pointer " onClick={() => router.push("/cart")}/>)
+                    : (
+                        <div className="flex gap-1 " onClick={() => router.push("/cart")}>
+                            <BaggageClaim strokeWidth={1} className="cursor-pointer" />
+                            <span>{cart.items.length}</span>
+                        </div>
+                    )
+
+                }
+                
                 <Heart className="cursor-pointer "  onClick={() => router.push("/love-products")}/>
                 <User strokeWidth={1} className="cursor-pointer"/>
                 <ToggleTheme />
